7f3b399c79c7f8024658fa2769da32d4b13c07a7
imtiazahammed1 <imtiaz2015160042@gmail.com>
2022-03-01 23:28:51 +0600

    all-added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const main = document.getElementById("main");
+const parent = document.getElementById("parent");
 
 // Load the Data from api
 const loadData = () => {
@@ -46,7 +47,7 @@ const errorHandle = info => {
 const displayPhones = (mobiles) => {
     const phones = mobiles.slice(0, 20);
     for (const phone of phones) {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement("div");
         div.classList.add("col-lg-4");
         div.classList.add("col-sm-6");
@@ -57,17 +58,58 @@ const displayPhones = (mobiles) => {
         <div style= "margin-left: 70px; margin-bottom: 30px">
             <div class="card" style="width: 16rem; border: 5px solid gray; border-radius: 15px">
                     <img src="${phone.image}" class="card-img-top mt-3 px-3" alt=" ">
-                <div class="card-body">
-                    <h5 class="card-title">${phone.brand}</h5>
-                    <p class="card-text">${phone.phone_name}</p>
-                    <button onclick="displayDetails('${phone.slug}')" class="btn btn-primary rounded-pill">See Details</button>
+                <div class="card-body text-center">
+                    <h5 class="card-title ">Brand: ${phone.brand}</h5>
+                    <h6 class="card-text ">Name: ${phone.phone_name}</h6>
+                    <button onclick="displayDetails('${phone.slug}')" class="btn btn-primary rounded-pill ">See Details</button>
                 </div>
             </div>
         </div>
             `
         main.appendChild(div);
 
-
     }
+
+}
+
+
+
+
+
+
+const displayDetails = (slug) => {
+
+    fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        .then(response => response.json())
+        .then(data => {
+            const div = document.createElement("div");
+            div.classList.add("col-sm-12");
+            div.innerHTML = `
+            <div style= "margin-bottom: 30px; margin-top: 30px; display:center;">
+                <div class="card" style="width: 30rem; border: 5px solid gray; border-radius: 15px">
+                    <img src="${data.data.image}" class="card-img-top mt-3 px-3" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">${data.data.slug}</h4>
+                        <p class="card-text">Release Date: ${data.data.releaseDate}</p>
+                        <p class="card-text">Storage: ${data.data.mainFeatures.storage}</p>
+                        <p class="card-text">Display Size: ${data.data.mainFeatures.displaySize}</p>
+                        <p class="card-text">Chipset: ${data.data.mainFeatures.chipSet}</p>
+                        <p class="card-text">Memory: ${data.data.mainFeatures.memory}</p>
+                        <p class="card-text">Sensors(i): ${data.data.mainFeatures.sensors[2]}</p>
+                        <p class="card-text">Sensors(ii): ${data.data.mainFeatures.sensors[5]}</p>
+                        <p class="card-text">Others(i): ${data.data.others.Bluetooth}</p>
+                        <p class="card-text">Others(ii): ${data.data.others.GPS}</p>
+                        <p class="card-text">Others(iii): ${data.data.others.USB}</p>
+                        <p class="card-text">Others(iv): ${data.data.others.WLAN}</p>
+
+                    </div>
+                </div>
+            </div>
+            
+            `
+            parent.appendChild(div);
+
+        })
+
 
 }
