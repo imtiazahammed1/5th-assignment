2c5cf609ee2158fdc139557cd16bf1a4aec3c498
imtiazahammed1 <imtiaz2015160042@gmail.com>
2022-03-01 18:50:29 +0600

    card-added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+const main = document.getElementById("main");
+
+// Load the Data from api
 const loadData = () => {
     const search = document.getElementById("search-text");
+    const error = document.getElementById("error");
     const searchText = search.value;
     if (searchText == "") {
         error.innerHTML = "*search can't be empty. Please write any text with phone name.";
@@ -22,6 +26,7 @@ const loadData = () => {
 
     }
 }
+// Error Handling for unknown search text
 const errorHandle = info => {
     if (info.status == true) {
         fetch(`https://openapi.programming-hero.com/api/phones?search=${info}`)
@@ -32,10 +37,33 @@ const errorHandle = info => {
         error.innerHTML = "*oops! no match found with this phone name.";
     }
 }
+// Dispaly Phones in coloumn
+const displayPhones = (mobiles) => {
+    // console.log(phones);
+    const phones = mobiles.slice(0, 20);
+    for (const phone of phones) {
+        console.log(phone);
+        const div = document.createElement("div");
+        div.classList.add("col-lg-4");
+        div.classList.add("col-sm-6");
+        div.classList.add("col-xs-12");
+        div.classList.add("col-md-4");
+        div.classList.add("my-3");
+        div.innerHTML = `
+        <div style= "margin-left: 70px;">
+            <div class="card" style="width: 16rem; border: 5px solid gray; border-radius: 15px">
+                    <img src="${phone.image}" class="card-img-top mt-3 px-3" alt=" ">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.brand}</h5>
+                    <p class="card-text">${phone.phone_name}</p>
+                    <button onclick="displayDetails('${phone.slug}')" class="btn btn-primary rounded-pill">See Details</button>
+                </div>
+            </div>
+        </div>
+            `
+        main.appendChild(div);
 
-const displayPhones = (phones) => {
-    console.log(phones);
-    const phone = phones.slice(0, 20);
-    console.log(phone);
+
+    }
 
 }
